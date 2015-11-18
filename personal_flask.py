6062ef72d54e0861c8b8a457a9e9f8e1a629baee
David Karapetyan <david.karapetyan@gmail.com>
2015-11-18 15:01:30 -0500

    Working personal website, now in flask

--- personal_flask.py
+++ personal_flask.py
@@ -1,12 +1,32 @@
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__, static_folder='static', template_folder='templates')
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return flask.render_template("layout.html", page_to_insert="about.html")
+
+
+@app.route('/about')
+def about():
+    return flask.render_template("layout.html", page_to_insert="about.html")
+
+
+@app.route('/test')
+def test():
+    return flask.render_template("layout.html", page_to_insert="about.html")
+
+
+@app.route('/publications')
+def publications():
+    return flask.send_file("static/pdfs/publications.pdf")
+
+
+@app.route('/resume')
+def resume():
+    return flask.send_file("static/pdfs/resume.pdf")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
